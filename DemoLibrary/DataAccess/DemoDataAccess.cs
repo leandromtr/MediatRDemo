dee795a8e9e54fe95228e5fdcc134b0f48afbe35
leandro <leandromtr@hotmail.com>
2021-04-09 08:49:09 +0100

    Add Mediator plugin and populate classes of Person

---
+++ DemoLibrary/DataAccess/DemoDataAccess.cs
@@ -0,0 +1,33 @@
+﻿using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.DataAccess
+{
+    public class DemoDataAccess : IDemoDataAccess
+    {
+        private List<PersonModel> people = new();
+
+        public DemoDataAccess()
+        {
+            people.Add(new PersonModel { Id = 1, FirstName = "Leandro", LastName = "Reis" });
+            people.Add(new PersonModel { Id = 2, FirstName = "Lucas", LastName = "Souza" });
+        }
+
+        public IList<PersonModel> GetPeople()
+        {
+            return people;
+        }
+
+        public PersonModel InsertPerson(string firstName, string lastName)
+        {
+            PersonModel person = new() { FirstName = firstName, LastName = lastName };
+            person.Id = people.Max(x => x.Id) + 1;
+            people.Add(person);
+            return person;
+        }
+    }
+}
---
+++ DemoLibrary/DataAccess/IDemoDataAccess.cs
@@ -0,0 +1,11 @@
+﻿using DemoLibrary.Models;
+using System.Collections.Generic;
+
+namespace DemoLibrary.DataAccess
+{
+    public interface IDemoDataAccess
+    {
+        IList<PersonModel> GetPeople();
+        PersonModel InsertPerson(string firstName, string lastName);
+    }
+}
