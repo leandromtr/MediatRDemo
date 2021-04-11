735b846615b01aa34636bbf30818921837bed5fd
leandro <leandromtr@hotmail.com>
2021-04-11 18:22:09 +0100

    Implementing Person Insert

---
+++ DemoLibrary/Handlers/InsertPersonHandler.cs
@@ -0,0 +1,27 @@
+﻿using DemoLibrary.Commands;
+using DemoLibrary.Models;
+using DemoLibrary.DataAccess;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Handlers
+{
+    public class InsertPersonHandler : IRequestHandler<InsertPersonCommand, PersonModel>
+    {
+        private readonly IDemoDataAccess _data; 
+        public InsertPersonHandler(IDemoDataAccess data)
+        {
+            _data = data;
+        }
+
+        public Task<PersonModel> Handle(InsertPersonCommand request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_data.InsertPerson(request.FirstName, request.LastName));
+        }
+    }
+}
